cfe2ecaa293afc5e5e231662fe5dd0a6a266e602
ARTHUR <137641282+eMongolia3-0@users.noreply.github.com>
2023-08-02 23:31:33 +0800

    Munkhmandakh

---
+++ ghost.js
@@ -0,0 +1,34 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var code1 = document.getElementById("code1").value;
+    var code2 = document.getElementById("code2").value;
+
+    if(code1 === "Т" && code2 == "Я"){
+        if (username === "02231807" && password === "Mandahaa0318") {
+            window.location.href = "face.html";
+        } else {
+            alert("Таны нэвтрэх нэр эсвэл нууц үг буруу байна. Шалгаад дахин оролдоно уу!");
+        }
+    }
+    else{
+        alert("Таны нэвтрэх нэр эсвэл нууц үг буруу байна. Шалгаад дахин оролдоно уу!")
+    }
+
+
+    
+}
+var showButton = document.getElementById("showButton");
+showButton.addEventListener("click", function () {
+    var passwordInput = document.getElementById("password");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        showButton.classList.remove("fa-eye-slash");
+        showButton.classList.add("fa-eye");
+    } else {
+        passwordInput.type = "password";
+        showButton.classList.remove("fa-eye");
+        showButton.classList.add("fa-eye-slash");
+    }
+});
